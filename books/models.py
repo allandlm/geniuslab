22aede3dd5c36a43cf95c4c821d08cc16e2787e3
Allan Marques <allanlima00@gmail.com>
2024-10-29 13:03:01 -0300

    Add Book and Loan models

--- books/models.py
+++ books/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    isbn = models.CharField(max_length=13, unique=True)
+    publisher = models.CharField(max_length=255)
+    publication_year = models.PositiveIntegerField()
+    genre = models.CharField(max_length=100)
+    total_quantity = models.PositiveIntegerField()
+    available_quantity = models.PositiveIntegerField()
+
+    def __str__(self):
+        return f"{self.title} by {self.author}"
--- loans/models.py
+++ loans/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from users.models import User
+from books.models import Book
 
-# Create your models here.
+class Loan(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    loan_date = models.DateField(auto_now_add=True)
+    due_date = models.DateField()
+    return_date = models.DateField(null=True, blank=True)
+    status = models.CharField(max_length=10, choices=[('open', 'Aberto'), ('closed', 'Concluído')], default='open')
+
+    def __str__(self):
+        return f"Loan of {self.book.title} to {self.user.username}"
